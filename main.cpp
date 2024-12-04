2a337a949f57a4d32198742643dbf5f67cf25057
KorakinDaniil <korakindaniil66@gmail.com>
2024-12-04 14:33:04 +0500

    menu

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void displayMenu()
+{
+    cout << "Выберите категорию:\n";
+    cout << "1 - Фрукты\n";
+    cout << "2 - Животные\n";
+    cout << "3 - Страны\n";
+    cout << "0 - Выход\n";
+}
+
+int main()
+{
+    int category;
+
+    while(true)
+    {
+        displayMenu();
+        cout << "Введите номер категории: ";
+        cin >> category;
+
+        switch (category)
+        {
+            case 1:
+                cout << "Вы выбрали категорию: Фрукты.\n";
+                break;
+            case 2:
+                cout << "Вы выбрали категорию: Животные.\n";
+                break;
+            case 3:
+                cout << "Вы выбрали категорию: Страны.\n";
+                break;
+            case 0:
+                cout << "Выход из программы.\n";
+                return 0;
+            default:
+                cout << "Некорректный ввод. Пожалуйста, попробуйте снова.\n";
+        }
+        cout << endl;
+    }
+    return 0;
+}
